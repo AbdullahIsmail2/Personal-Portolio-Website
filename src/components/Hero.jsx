df52ac436b2c03a8dd84369d47d9df2de22328d6
Abdullah Ismail <abdullahmusa2003@gmail.com>
2024-01-14 16:22:02 +0000

    added main text on hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className="mt-[120px] min-h-screen w-full  ">
+			<div
+				className={`flex flex-row  gap-5 ${styles.paddingX} max-w-7xl m-auto`}
+			>
+				<div className="flex flex-col items-center mt-2">
+					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
+					<div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+				</div>
+				<div className="flex flex-col gap-4">
+					<h1 className={`${styles.heroHeadText}  text-white`}>
+						Hi, I'm <span className="text-[#915eff]">Abdullah</span>
+					</h1>
+					<h3 className={`${styles.heroSubText}`}>
+						A Front-End Developer crafting modern websites{" "}
+						<br className="hidden sm:block" /> with cutting-edge technologies
+						and stunning UIs
+					</h3>
+				</div>
+			</div>
 
-export default Hero
+      <ComputersCanvas />
+		</section>
+	);
+};
+
+export default Hero;
